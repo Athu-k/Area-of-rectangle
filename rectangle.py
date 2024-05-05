98d383e7f71c79eebda60d84aaff5910af3a9523
Athu-k <athulyamangalasseri@gmail.com>
2024-05-05 18:34:13 +0530

    commited changes

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+#!python3
+# Area-of-rectangle
+#First build automation
+def area_of_rectangle(length, breadth):
+        return length*breadth
+
+
+a=int(input("length "))
+b=int(input("breadth "))
+area = area_of_rectangle(a,b)
+
+print(f"area = {area}")
+print("Completed")
